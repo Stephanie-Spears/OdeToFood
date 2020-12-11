d428a903250aae4564352d8ecd3c7261690d597a
Stephanie Spears <stephanie.spears1@gmail.com>
2020-12-10 17:29:38 -0800

    add OdeToFood.Data Models and Services classes and interfaces

---
+++ OdeToFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,15 @@
+﻿using OdeToFood.Data.Models;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+	//add public to use this interface outside the odeToFood.Data Project
+	public interface IRestaurantData
+	{
+		IEnumerable<Restaurant> GetAll();
+	}
+}
---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,29 @@
+﻿using OdeToFood.Data.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+	public class InMemoryRestaurantData : IRestaurantData
+	{
+		List<Restaurant> restaurants;
+
+		public InMemoryRestaurantData()
+		{
+			restaurants = new List<Restaurant>()
+			{
+				new Restaurant {Id = 1, Name = "Rovente's Pizza", Cuisine = CuisineType.Italian},
+				new Restaurant {Id = 2, Name = "Le Pigeon", Cuisine = CuisineType.French},
+				new Restaurant {Id = 3, Name = "Bollywood Theater", Cuisine = CuisineType.Indian}
+			};
+		}
+
+		public IEnumerable<Restaurant> GetAll()
+		{
+			//Using LINQ OrderBy, given a restaurant r, order by restaunt name in ascending order
+			return restaurants.OrderBy(r => r.Name);
+		}
+	}
+}
