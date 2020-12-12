3b26aec54ed077f0c21ba3ffb208cdbc5e844e56
Stephanie Spears <stephanie.spears1@gmail.com>
2020-12-11 18:36:37 -0800

    return xml IEnumerable of Data.Models

---
+++ OdeToFood.Web/Api/Restaurant.cs
@@ -0,0 +1,6 @@
+﻿namespace OdeToFood.Web.Api
+{
+	public class Restaurant
+	{
+	}
+}
---
+++ OdeToFood.Web/Api/RestaurantsController.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OdeToFood.Web.Api
+{
+    public class RestaurantsController : ApiController
+    {
+		private readonly IRestaurantData db;
+
+		public RestaurantsController(IRestaurantData db)
+		{
+			this.db = db;
+		}
+        public IEnumerable<Data.Models.Restaurant> Get()
+		{
+			var model = db.GetAll();
+			return model;
+		}
+    }
+}
