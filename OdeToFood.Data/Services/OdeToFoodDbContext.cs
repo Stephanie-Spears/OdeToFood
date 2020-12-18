f6a97cc35798bcfdabcb086ad40c0e60cd271292
Stephanie Spears <stephanie.spears1@gmail.com>
2020-12-18 15:11:20 -0800

    add localdb connection'

---
+++ OdeToFood.Data/Services/OdeToFoodDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+	// This is the gateway to the database, we can configure which database we want to point to by adding a connection string in web.config
+	public class OdeToFoodDbContext :DbContext
+	{
+		public DbSet<Restaurant> Restaurants { get; set; }
+	}
+}
