09e67dd08ffd507f85186d6ac64f721f89c4de3b
Stephanie Spears <stephanie.spears1@gmail.com>
2020-12-19 11:15:14 -0800

    add partials and tempdata to pass data between controller actions and across requests

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -76,7 +76,8 @@ namespace OdeToFood.Web.Controllers
         {
 	        if (ModelState.IsValid)
 	        {
-		        db.Update(restaurant);	
+		        db.Update(restaurant);
+		        TempData["Message"] = "You have saved the restaurant!";
 		        return RedirectToAction("Details", new {id = restaurant.Id});
 	        }
 
